d322dae9e8d4967a00e90da875ab9e8e3d83524f
Aaron Hammond <ahammond@mit.edu>
2014-05-27 20:06:22 -0400

    probably the only commmit that will be necessary

---
+++ routes/user.js
@@ -0,0 +1,71 @@
+var User = require('../models/User');
+var passport = require('../util/passport');
+var mongoose = require('mongoose');
+
+
+/* 
+ * GET /user/dashboard
+ */
+
+ exports.viewDashboard = function(req, res){
+ 	if(!req.user){
+		return res.redirect('/user/login');
+	}
+ 	res.render('userDashboard', {msg: req.flash('error')});
+ }
+
+
+/* 
+ * GET /user/register
+ */
+
+exports.viewRegister = function(req, res){
+ 	res.render('userRegister', { msg: req.flash('error') });
+ }
+
+/* 
+ * POST /user/register
+ */
+exports.doRegister = function(req, res){
+	User.findOne({username: req.param('username')}, function(err, user) {
+		if(user){
+			req.flash('error', 'An account with that username already exists!');
+			return res.redirect('/user/register');
+		}
+
+		user = new User({	username: req.param('username'),
+							password: req.param('password')});
+		user.save(function (err, docuser){
+			if(err){
+	    		req.flash('error', err);
+	    		return res.redirect('/user/register');
+			}
+
+			req.logIn(docuser, function(err){
+				console.log("logged in after registration");
+			    if(err){
+			        req.flash('error', 'Your account was created. Please log in.');
+			        return res.redirect('/user/login');
+			    }
+
+				return res.redirect('/user/dashboard');
+			});
+		});
+	});
+}
+
+/* 
+ * GET /user/login
+ */
+
+exports.viewLogin = function(req, res){
+	res.render('userLogin', { msg: req.flash('error') });
+}
+
+/* 
+ * POST /user/login
+ */
+ exports.doLogin = passport.authenticate('local', {successRedirect: '/user/dashboard',
+        failureRedirect: '/user/login',
+        failureFlash: true
+    });
---
+++ util/passport.js
@@ -0,0 +1,38 @@
+var flash = require('connect-flash');
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    User = require('../models/User');
+
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findOne({_id: id}, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({username: username}, function(err, user) {
+            if(err){
+                return done(err);
+            }
+            if(!user) {
+                console.log("Bad username");
+                return done(null, false, {message: 'Incorrect Username'});
+            }
+            return user.comparePassword(password, function(err, isMatch){
+                if(err || !isMatch){
+                    console.log("Bad pass");
+                    return done(null, false, {message: 'Incorrect Password'});
+                }
+                return done(null, user);
+            })
+
+        });
+    }
+));
+
+module.exports = passport;
